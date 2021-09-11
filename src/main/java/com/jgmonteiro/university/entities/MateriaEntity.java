a14940624feaba043d8f866f6d223183fb3bbca1
jgmonteiro1 <jgmoreira15@gmail.com>
2021-09-11 15:03:17 -0300

    Entidade + Controle bases criados

---
+++ src/main/java/com/jgmonteiro/university/entities/MateriaEntity.java
@@ -0,0 +1,45 @@
+package com.jgmonteiro.university.entities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@Entity
+@Table(name = "tb_materia")
+@Data
+@NoArgsConstructor
+public class MateriaEntity implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @JsonInclude(Include.NON_NULL)
+    @Column(name = "id")
+    private Long id;
+
+    @JsonInclude(Include.NON_EMPTY)
+    @Column(name = "nome")
+    private String name;
+
+    @Column(name = "hrs")
+    private Integer horas;
+
+    @JsonInclude(Include.NON_EMPTY)
+    @Column(name = "cod")
+    private String codigo;
+
+    @JsonInclude(Include.NON_EMPTY)
+    @Column(name = "freq")
+    private Integer frequencia;
+
+
+
+}
--- src/main/java/com/jgmonteiro/university/resources/MateriaResource.java
+++ src/main/java/com/jgmonteiro/university/resources/MateriaResource.java
@@ -1,18 +1,37 @@
 package com.jgmonteiro.university.resources;
 
+import com.jgmonteiro.university.entities.MateriaEntity;
+import com.jgmonteiro.university.repository.MateriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/materias")
 public class MateriaResource {
 
-    @GetMapping("/")
-    public ResponseEntity<String> getTest(){
-        return ResponseEntity.status(HttpStatus.OK).body("Teste");
+    @Autowired
+    private MateriaRepository materiaRepository;
+
+    @GetMapping
+    public ResponseEntity<List<MateriaEntity>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(materiaRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<MateriaEntity>> getById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(materiaRepository.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity createMateria(@RequestBody MateriaEntity materiaEntity){
+        materiaRepository.save(materiaEntity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(true);
     }
 
 }
