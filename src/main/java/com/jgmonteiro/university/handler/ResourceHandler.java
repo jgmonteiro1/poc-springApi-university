2dbf3b95b3344da86f5328070963193b330f3cb9
jgmonteiro1 <jgmoreira15@gmail.com>
2021-09-15 00:31:21 -0300

    Tratando o Valid para retornar um Map de erros

--- src/main/java/com/jgmonteiro/university/handler/ResourceHandler.java
+++ src/main/java/com/jgmonteiro/university/handler/ResourceHandler.java
@@ -2,9 +2,15 @@ package com.jgmonteiro.university.handler;
 
 import com.jgmonteiro.university.exceptions.MateriaException;
 import com.jgmonteiro.university.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ResourceHandler {
@@ -17,5 +23,19 @@ public class ResourceHandler {
                 .timeStamp(System.currentTimeMillis())
                 .build();
         return ResponseEntity.status(exception.getHttpStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handlerValidException(MethodArgumentNotValidException exception){
+
+        Map<String, String> erros = new HashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach(erro -> {
+            String campo = ((FieldError) erro).getField();
+            String mensagem = erro.getDefaultMessage();
+            erros.put(campo, mensagem);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
     }
 }
